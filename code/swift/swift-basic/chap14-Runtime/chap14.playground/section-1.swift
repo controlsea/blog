f6786ea3f267e978cb10c69122db754e23f3bafb
akadealloc <jayson.xu@foxmail.com>
2014-12-25 23:50:04 +0800

    update

--- code/swift/swift-basic/chap14-Runtime/chap14.playground/section-1.swift
+++ code/swift/swift-basic/chap14-Runtime/chap14.playground/section-1.swift
@@ -1,26 +1,29 @@
 // Playground - noun: a place where people can play
 
-import UIKit
+import Darwin
 import ObjectiveC
+import Foundation
 
 var str = "Hello, playground"
 
 
 class MyClass
 {
-    func test()
-    {
-        println("hello");
-    }
-    
-    func swizzledTest()
-    {
-        println("swizzled")
-    }
-
+    let a : UInt32 = 10
+    func test(){ }
 }
 
-var c = MyClass()
-c.test()
+var obj = MyClass()
+var ptr:UnsafePointer<Void> = unsafeAddressOf(obj)
+let mutable_ptr = UnsafeMutablePointer<Void>(ptr)
+let l:UInt = malloc_size(mutable_ptr)
+let d = NSData(bytes: UnsafePointer<Void>(mutable_ptr), length: (Int)(l));
+println("%@",d)
+
+
+println("%@",class_getSuperclass(MyClass))
+println("%@",objc_getClass(UnsafePointer<Int8>(mutable_ptr).memory))
+
+free(mutable_ptr)
 
 
---
+++ code/swift/swift-basic/chap15-Initialization/chap15.playground/section-1.swift
@@ -0,0 +1,63 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//initialization
+
+//one single rule : Every value must be initialized before it is used
+class B
+{
+    func foo() -> Void
+    {
+        
+    }
+}
+
+//call super.init at the bottom of init method
+class C : B
+{
+    lazy var color = UIColor.whiteColor()
+    
+    var hasTurbo :Bool
+    
+    override func foo() {
+        
+        hasTurbo = false
+        println("turbo:\(hasTurbo)")
+    }
+    
+    //designated initializer
+    //can be inherited
+    init(turbo:Bool) {
+        
+        hasTurbo = turbo
+        
+        super.init() //call super.init() after hasTurbo has been initialized
+    }
+    
+    //convenient initializer
+    //can not be inherited
+    convenience init(color:UIColor) {
+        
+        self.init(turbo:true)
+        self.color = color
+    }
+    
+    
+    //deinitializer
+    deinit
+    {
+        println("C has been dealloced")
+        
+    }
+    
+}
+
+func testDeinit()
+{
+    var obj:C = C(color: UIColor.blackColor())
+    obj.foo()
+    
+}
+
+testDeinit()
